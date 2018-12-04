e92f1f8644467d2ffb0d822e1f50b543858f75b9
Igor Kim <igkim90@gmail.com>
2018-12-04 14:55:16 +0500

    socket

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import '../assets/application.css';
 // import faker from 'faker';
 import gon from 'gon';
 // import cookies from 'js-cookie';
-// import io from 'socket.io-client';
+import io from 'socket.io-client';
 
 import app from './jsx/index.jsx';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
+
+const socket = io('http://igakim-slack.herokuapp.com');
+socket.on('connect', () => console.log('testing'));
 
 app(gon);
