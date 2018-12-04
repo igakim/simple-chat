4626225fb256f45d8f6dfeb24d738425702fbbb7
Igor Kim <igkim90@gmail.com>
2018-12-04 15:41:30 +0500

    add sockets

--- src/index.js
+++ src/index.js
@@ -12,7 +12,9 @@ if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-const socket = io('http://igakim-slack.herokuapp.com');
+const socket = io();
 socket.on('connect', () => console.log('testing'));
+socket.on('event', data => console.log(data));
+socket.on('disconnect', () => console.log('disconnect'));
 
 app(gon);
