57af814e51f7e47926a269c4aab9693369230001
Igor Kim <igkim90@gmail.com>
2018-12-06 18:43:40 +0500

    test codeclimate

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,3 +1,9 @@
 test('example', () => {
+  const expected = 'test';
+  expect('test').toBe(expected);
+});
 
+test('example2', () => {
+  const expected = 'test2';
+  expect('test2').toBe(expected);
 });
