86fb52b7e0f8e33120c3088890d8a383b7d285f1
Igor Kim <igkim90@gmail.com>
2018-12-06 23:39:06 +0500

    remove tests

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,11 +1,4 @@
-import routes from '../src/routes';
 
 test('example', () => {
-  const expected = '/api/v1/channels/0/messages';
-  expect(routes.postMessageUrl(0)).toBe(expected);
-});
 
-test('example2', () => {
-  const expected = '/api/v1/channels/1/messages';
-  expect(routes.postMessageUrl(1)).toBe(expected);
 });
