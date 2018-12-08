86d33209f374a891d3dba77a2086b927ce49e866
Igor Kim <igkim90@gmail.com>
2018-12-08 20:14:11 +0500

    add React context provider

--- src/app.jsx
+++ src/app.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './components/App';
+import { UserNameContext } from './userNameContext';
 
-export default (store) => {
+export default (store, userName) => {
   render(
     <Provider store={store}>
-      <App />
+      <UserNameContext.Provider value={userName}>
+        <App />
+      </UserNameContext.Provider>
     </Provider>,
     document.getElementById('chat'),
   );
