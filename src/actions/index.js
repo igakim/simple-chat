0b365c10aebad2ff425b83ad942119dbb8e87b31
Igor Kim <igkim90@gmail.com>
2018-12-08 20:13:50 +0500

    remove @babel-pollyfil change some actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,13 @@
 import { createAction } from 'redux-actions';
 // import { uniqueId } from 'lodash';
 import axios from 'axios';
-import '@babel/polyfill';
 import routes from '../routes';
 
 export const sendMessageRequest = createAction('MESSAGE_SEND_REQUEST');
 export const sendMessageSuccess = createAction('MESSAGE_SEND_SUCCESS');
 export const sendMessageFailure = createAction('MESSAGE_SEND_FAILURE');
 
-export const setUserName = createAction('USER_NAME_SET');
-
-export const sendMessage = (message, userName, currentChannelId) => async (dispatch) => {
+export const sendMessage = (message, userName, currentChannelId, callback) => async (dispatch) => {
   dispatch(sendMessageRequest());
   try {
     const data = {
@@ -23,9 +20,11 @@ export const sendMessage = (message, userName, currentChannelId) => async (dispa
       },
     };
 
-    axios.post(routes.postMessageUrl(currentChannelId), data);
-    // dispatch(sendMessageSuccess(response.data));
+    await axios.post(routes.postMessageUrl(currentChannelId), data);
+    callback();
+    // dispatch(sendMessageSuccess());
   } catch (e) {
     dispatch(sendMessageFailure());
+    callback();
   }
 };
