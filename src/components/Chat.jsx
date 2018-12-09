8b11b3996f38d6a3ba1ed5f7f384fe4dd0649469
Igor Kim <igkim90@gmail.com>
2018-12-10 00:31:35 +0500

    refactor with react-bootstrap components

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,21 +1,47 @@
 import React from 'react';
+import { Tab } from 'react-bootstrap';
+import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
 import MessageBox from './MessageBox';
 import MessageForm from './MessageForm';
 import MessageTitle from './MessageTitle';
 
+const getChannels = state => state.channels;
 
-const Chat = () => (
-  <div className="card">
-    <div className="card-header">
-      <MessageTitle />
-    </div>
-    <div className="card-body">
-      <MessageBox />
-    </div>
-    <div className="card-footer">
-      <MessageForm />
-    </div>
-  </div>
+const channelsList = createSelector(
+  getChannels,
+  ch => Object.values(ch),
 );
+
+const mapStateToProps = state => ({
+  channels: channelsList(state),
+  currentChannelId: state.currentChannelId,
+});
+
+@connect(mapStateToProps)
+class Chat extends React.Component {
+  render() {
+    const { channels } = this.props;
+    return (
+      <Tab.Content>
+        {channels.map(({ id }) => (
+          <Tab.Pane eventKey={id} key={id}>
+            <div className="card">
+              <div className="card-header">
+                <MessageTitle />
+              </div>
+              <div className="card-body">
+                <MessageBox />
+              </div>
+              <div className="card-footer">
+                <MessageForm />
+              </div>
+            </div>
+          </Tab.Pane>
+        ))}
+      </Tab.Content>
+    );
+  }
+}
 
 export default Chat;
