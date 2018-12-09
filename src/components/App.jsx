25d51959fd0474e0050754cfa5887ef82b055490
Igor Kim <igkim90@gmail.com>
2018-12-10 00:30:39 +0500

    refactor with react-bootstrap components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import { Tab, Row, Col } from 'react-bootstrap';
 import ChannelsList from './ChannelsList';
 import Chat from './Chat';
 
+
 const App = () => (
-  <div className="row">
-    <div className="col-lg-3">
-      <ChannelsList />
-    </div>
-    <div className="col-lg-9">
-      <Chat />
-    </div>
-  </div>
+  <Tab.Container id="chat" defaultActiveKey="1">
+    <Row>
+      <Col sm={3}>
+        <ChannelsList />
+      </Col>
+      <Col sm={9}>
+        <Chat />
+      </Col>
+    </Row>
+  </Tab.Container>
 );
 
 export default App;
