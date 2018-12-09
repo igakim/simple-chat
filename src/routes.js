ee77af5246358cba6031873df5688362e5a2d94c
Igor Kim <igkim90@gmail.com>
2018-12-10 00:34:16 +0500

    add routes

--- src/routes.js
+++ src/routes.js
@@ -2,4 +2,6 @@ const host = '';
 // api/v1/channels/0/messages
 export default {
   postMessageUrl: id => [host, 'api/v1/channels', id, 'messages'].join('/'),
+  channels: () => [host, 'api/v1/channels'].join('/'),
+  theChannelUrl: id => [host, 'api/v1/channels', id].join('/'),
 };
