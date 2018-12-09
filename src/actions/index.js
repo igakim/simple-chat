df0bca3b39acbf02e656a317d9cf9bc8132ce116
Igor Kim <igkim90@gmail.com>
2018-12-10 00:29:42 +0500

    add remove, rename and add channel actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -28,3 +28,62 @@ export const sendMessage = (message, userName, currentChannelId, callback) => as
     callback();
   }
 };
+//   { id: generalChannelId, name: 'general', removable: false },
+export const changeChannelId = createAction('CHANNEL_ID_CHANGE');
+
+export const addChannelRequest = createAction('CHANNEL_ADD_REQUEST');
+export const addChannelSuccess = createAction('CHANNEL_ADD_SUCCESS');
+export const addChannelFailure = createAction('CHANNEL_ADD_FAILURE');
+
+export const addChannel = (name, callback) => async (dispatch) => {
+  dispatch(addChannelRequest());
+  try {
+    const data = {
+      data: {
+        attributes: {
+          name,
+        },
+      },
+    };
+    await axios.post(routes.channels(), data);
+    callback();
+  } catch (e) {
+    dispatch(addChannelFailure());
+    callback();
+  }
+};
+
+export const renameChannelRequest = createAction('CHANNEL_RENAME_REQUEST');
+export const renameChannelSuccess = createAction('CHANNEL_RENAME_SUCCESS');
+export const renameChannelFailure = createAction('CHANNEL_RENAME_FAILURE');
+
+export const renameChannel = (name, id, callback) => async (dispatch) => {
+  dispatch(renameChannelRequest());
+  try {
+    const data = {
+      data: {
+        attributes: {
+          name,
+        },
+      },
+    };
+
+    await axios.patch(routes.theChannelUrl(id), data);
+    callback();
+  } catch (e) {
+    dispatch(renameChannelFailure());
+  }
+};
+
+export const removeChannelRequest = createAction('CHANNEL_REMOVE_REQUEST');
+export const removeChannelSuccess = createAction('CHANNEL_REMOVE_SUCCESS');
+export const removeChannelFailure = createAction('CHANNEL_REMOVE_FAILURE');
+
+export const removeChannel = id => async (dispatch) => {
+  dispatch(removeChannelRequest());
+  try {
+    axios.delete(routes.theChannelUrl(id));
+  } catch (e) {
+    dispatch(removeChannelFailure());
+  }
+};
