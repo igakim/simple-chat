4da3586fc9bf3a86dedb6df481d227d2936d04c1
Igor Kim <igkim90@gmail.com>
2018-12-10 19:51:15 +0500

    refactor async actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,11 +3,14 @@ import { createAction } from 'redux-actions';
 import axios from 'axios';
 import routes from '../routes';
 
+export const showAlert = createAction('NOTIFICATION_SHOW');
+export const closeAlert = createAction('NOTIFICATION_CLOSE');
+
 export const sendMessageRequest = createAction('MESSAGE_SEND_REQUEST');
 export const sendMessageSuccess = createAction('MESSAGE_SEND_SUCCESS');
 export const sendMessageFailure = createAction('MESSAGE_SEND_FAILURE');
 
-export const sendMessage = (message, userName, currentChannelId, callback) => async (dispatch) => {
+export const sendMessage = (message, userName, currentChannelId) => async (dispatch) => {
   dispatch(sendMessageRequest());
   try {
     const data = {
@@ -21,11 +24,15 @@ export const sendMessage = (message, userName, currentChannelId, callback) => as
     };
 
     await axios.post(routes.postMessageUrl(currentChannelId), data);
-    callback();
-    // dispatch(sendMessageSuccess());
   } catch (e) {
     dispatch(sendMessageFailure());
-    callback();
+    dispatch(showAlert({
+      alertProperty: {
+        title: 'Message wasn\'t sent.',
+        variant: 'danger',
+        description: `Reason: ${e.message}. Try again later!`,
+      },
+    }));
   }
 };
 //   { id: generalChannelId, name: 'general', removable: false },
@@ -35,44 +42,64 @@ export const addChannelRequest = createAction('CHANNEL_ADD_REQUEST');
 export const addChannelSuccess = createAction('CHANNEL_ADD_SUCCESS');
 export const addChannelFailure = createAction('CHANNEL_ADD_FAILURE');
 
-export const addChannel = (name, callback) => async (dispatch) => {
+export const addChannel = name => async (dispatch) => {
   dispatch(addChannelRequest());
-  try {
-    const data = {
-      data: {
-        attributes: {
-          name,
-        },
+  const data = {
+    data: {
+      attributes: {
+        name,
       },
-    };
-    await axios.post(routes.channels(), data);
-    callback();
-  } catch (e) {
-    dispatch(addChannelFailure());
-    callback();
-  }
+    },
+  };
+  await axios.post(routes.channels(), data)
+    .then(() => {
+      dispatch(showAlert({ alertProperty: { title: 'Channel added!', variant: 'success' } }));
+    }).catch((e) => {
+      dispatch(addChannelFailure());
+      dispatch(showAlert({
+        alertProperty: {
+          title: 'Channel wasn\'t added',
+          description: `Reason: ${e.message}. Try again later`,
+          variant: 'danger',
+        },
+      }));
+    });
 };
 
 export const renameChannelRequest = createAction('CHANNEL_RENAME_REQUEST');
 export const renameChannelSuccess = createAction('CHANNEL_RENAME_SUCCESS');
 export const renameChannelFailure = createAction('CHANNEL_RENAME_FAILURE');
 
-export const renameChannel = (name, id, callback) => async (dispatch) => {
+export const renameChannel = (name, id) => async (dispatch) => {
   dispatch(renameChannelRequest());
-  try {
-    const data = {
-      data: {
-        attributes: {
-          name,
-        },
+  const data = {
+    data: {
+      attributes: {
+        name,
       },
-    };
+    },
+  };
 
-    await axios.patch(routes.theChannelUrl(id), data);
-    callback();
-  } catch (e) {
-    dispatch(renameChannelFailure());
-  }
+  await axios.patch(routes.theChannelUrl(id), data)
+    .then(() => {
+      dispatch(showAlert({
+        alertProperty: {
+          title: 'Channel name was changed!',
+          variant: 'success',
+          description: `New channel name is '${name}'`,
+        },
+      }));
+    })
+    .catch((e) => {
+      dispatch(renameChannelFailure());
+      dispatch(showAlert({
+        alertProperty: {
+          title: 'Channel wasn\'t renamed',
+          variant: 'danger',
+          description: `Reason: ${e.message}. Try again later`,
+        },
+      }));
+    });
 };
 
 export const removeChannelRequest = createAction('CHANNEL_REMOVE_REQUEST');
@@ -81,9 +108,23 @@ export const removeChannelFailure = createAction('CHANNEL_REMOVE_FAILURE');
 
 export const removeChannel = id => async (dispatch) => {
   dispatch(removeChannelRequest());
-  try {
-    axios.delete(routes.theChannelUrl(id));
-  } catch (e) {
-    dispatch(removeChannelFailure());
-  }
+  axios.delete(routes.theChannelUrl(id))
+    .then(() => {
+      dispatch(showAlert({
+        alertProperty: {
+          title: 'Channel removed!',
+          variant: 'success',
+        },
+      }));
+    })
+    .catch((e) => {
+      dispatch(removeChannelFailure());
+      dispatch(showAlert({
+        alertProperty: {
+          title: 'Channel wasn\'t removed!',
+          variant: 'danger',
+          description: `Reason: ${e.message}. Try again later!`,
+        },
+      }));
+    });
 };
