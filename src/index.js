72a656098d60547ac22fac5eff35a3cc8ecfae7c
Igor Kim <igkim90@gmail.com>
2018-12-15 12:55:55 +0500

    refactor importing faker

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import '@babel/polyfill';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { keyBy } from 'lodash';
 import thunk from 'redux-thunk';
-import faker from 'faker';
+import faker from 'faker/locale/en_US';
 import gon from 'gon';
 import cookies from 'js-cookie';
 import io from 'socket.io-client';
